a75e547335875b7bb67ef5e70889fb0da95c47a2
Cyopn <calvillobenitezjuanantonio@gmail.com>
2025-11-20 13:48:25 -0600

    fix(integrity)

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -5,10 +5,11 @@ const Item = sequelize.define('item', {
     id_item: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        references: { model: "product", key: "id_product" }
+        references: { model: "product", key: "id_product" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
     },
     name: { type: DataTypes.STRING, allowNull: false },
-    mount: { type: DataTypes.FLOAT, allowNull: false },
-}, { tableName: 'item', timestamps: true });
+}, { tableName: 'item', timestamps: true, underscored: true });
 
 export default Item;
