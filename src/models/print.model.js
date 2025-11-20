184054cd8fa6bb4a0e3856718b8117d153fbd468
Cyopn <calvillobenitezjuanantonio@gmail.com>
2025-11-20 13:50:12 -0600

    fix(print attr)

--- src/models/print.model.js
+++ src/models/print.model.js
@@ -5,10 +5,17 @@ const Print = sequelize.define("print", {
     id_print: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        references: { model: "product", key: "id_product" }
+        references: { model: "product", key: "id_product" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
     },
-    type_print: { type: DataTypes.STRING, allowNull: false },
-    type_paper: { type: DataTypes.STRING, allowNull: false },
-}, { tableName: "print", timestamps: true });
+    print_type: { type: DataTypes.STRING, allowNull: false },
+    paper_type: { type: DataTypes.STRING, allowNull: false },
+    paper_size: { type: DataTypes.STRING, allowNull: false },
+    range: { type: DataTypes.STRING, allowNull: false },
+    both_sides: { type: DataTypes.BOOLEAN, allowNull: false },
+    amount: { type: DataTypes.INTEGER, allowNull: false, validate: { min: 0 } },
+    observations: { type: DataTypes.TEXT, allowNull: true },
+}, { tableName: "print", timestamps: true, underscored: true });
 
 export default Print;
